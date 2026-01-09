d2f219a357c5c717e4d1737d186448f05b3b2fb4
2022bcs0181-keerthi <keerthimukkara1818@gmail.com>
2026-01-09 22:48:38 +0530

    Experiment 3: Lasso Regression alpha=0.1

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-model = LinearRegression()
+model = Lasso(alpha=0.1)
 model.fit(X_train, y_train)
 
 # Prediction
