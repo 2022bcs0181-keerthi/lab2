0c981963431f8b603d5d2dd682d9aec2b1112c9b
2022bcs0181-keerthi <keerthimukkara1818@gmail.com>
2026-01-09 22:50:33 +0530

    Experiment 4: Ridge Regression alpha=1.0

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-model = Lasso(alpha=0.1)
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # Prediction
